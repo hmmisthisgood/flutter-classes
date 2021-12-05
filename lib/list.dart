e81c8c4e4e797c5337bdadf10241c7fe526d47bd
surpriso1997 <maneesh10chaulagain@gmail.com>
2021-12-05 07:26:33 +0545

    created new details screen and added navigation to it

--- lib/list.dart
+++ lib/list.dart
@@ -1,3 +1,5 @@
+import 'package:android_and_ios/new_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ListLearning extends StatefulWidget {
@@ -14,6 +16,15 @@ class _ListLearningState extends State<ListLearning> {
     setState(() {
       ourColor = Colors.green;
     });
+
+    /// to go to new page we have to use, push funtion.
+    Navigator.push(
+      context,
+      CupertinoPageRoute(builder: (_) {
+        /// this is returning the route/screen/page we are trying to navigate to
+        return PostDetailsScreen();
+      }),
+    );
   }
 
   @override
@@ -82,71 +93,76 @@ class _ListLearningState extends State<ListLearning> {
           var post = posts[index];
 
           /// addding padding the to container from all sides
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(10),
+          return InkWell(
+            onTap: () {
+              onButtonPress();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(10),
 
-              /// to decorate the container
-              decoration: BoxDecoration(
-                /// give the color to the container
-                color: Colors.white,
+                /// to decorate the container
+                decoration: BoxDecoration(
+                  /// give the color to the container
+                  color: Colors.white,
 
-                /// making the container rounded
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
-                border: Border.all(color: Colors.grey, width: 1),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black45,
-                      spreadRadius: 5,
-                      blurRadius: 10,
-                      offset: Offset(10, 10))
-                ],
-              ),
+                  /// making the container rounded
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
+                  border: Border.all(color: Colors.grey, width: 1),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black45,
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: Offset(10, 10))
+                  ],
+                ),
 
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      post,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: ourColor),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        post,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: ourColor),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Image.asset("assets/images/solo.jpeg"),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.send,
-                              color: ourColor,
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.thumbs_up_down,
-                              color: ourColor,
-                            ),
-                            SizedBox(width: 10),
-                            Icon(Icons.comment, color: ourColor),
-                          ],
-                        ),
-                        Icon(Icons.share)
-                      ],
-                    ),
-                  )
-                ],
+                    SizedBox(height: 10),
+                    Image.asset("assets/images/solo.jpeg"),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.send,
+                                color: ourColor,
+                              ),
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.thumbs_up_down,
+                                color: ourColor,
+                              ),
+                              SizedBox(width: 10),
+                              Icon(Icons.comment, color: ourColor),
+                            ],
+                          ),
+                          Icon(Icons.share)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
