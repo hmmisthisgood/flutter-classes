e4eb7f2f0fdcd873529380a7fdb06a488a04c669
surpriso1997 <maneesh10chaulagain@gmail.com>
2021-12-05 07:27:53 +0545

    added new screen

---
+++ lib/new_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PostDetailsScreen extends StatelessWidget {
+  const PostDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+        title: Text("Details Screen"),
+      ),
+    );
+  }
+}
